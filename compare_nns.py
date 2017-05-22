eb697791a7acdfd4a2dbb79b9beda8aa7df47b65
Leo <leo.van.nierop@gmail.com>
2017-05-22 23:33:48 +0200

    saving and restoring functionality added

--- compare_nns.py
+++ compare_nns.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-from tensorflow_network_creator import Tensorflow_neural_network
+from tensorflow_network_creator import TensorflowNeuralNetwork
 from datetime import datetime
 from sklearn.utils import shuffle
+import sys
 
 print("imports done: {}".format(datetime.now()))
 
@@ -28,6 +29,10 @@ labels_train = labels[int(test_proportion * images.shape[0]):]
 
 print("data loaded: {}".format(datetime.now()))
 
+global_step = tf.Variable(0, trainable=False)
+learning_rate = tf.train.exponential_decay(0.5, global_step,
+                                           images_train.shape[0]/20, 0.9, staircase=True)
+
 convolution_model = {
     'input_shape': [None, 28, 28, 1],
     'output_shape':  [None, 10],
@@ -41,7 +46,7 @@ convolution_model = {
         },
         {
             'type': 'activation',
-            'function': tf.nn.relu
+            'function': "relu"
         },
         {
             'type': 'pool',
@@ -59,7 +64,7 @@ convolution_model = {
         },
         {
             'type': 'activation',
-            'function': tf.nn.relu
+            'function': "relu"
         },
         {
             'type': 'reshape',
@@ -75,67 +80,56 @@ convolution_model = {
         }
     ],
     'train_settings': {
-        'optimizer': tf.train.GradientDescentOptimizer(0.5),
-        'loss_function': tf.nn.softmax_cross_entropy_with_logits,
-        'reducer': tf.reduce_mean
+        'optimizer': "GradientDescent",
+        'learning_rate': {
+            "base": 0.5,
+            "decay": "exponential",
+            "decay_args": {
+                "learning_rate": 0.5,
+                "decay_steps": images_train.shape[0]/20,
+                "decay_rate": 0.9,
+                "staircase": True
+            }
+        },
+        'loss_function': "softmax_cross_entropy",
+        'reducer': "mean"
     },
-    'prediction_activation': tf.nn.softmax
+    'prediction_activation': "softmax"
 }
 
-simple_nn_model = {
-    'input_shape': [None, 784],
-    'output_shape':  [None, 10],
-    'layers': [
-        {
-            'type': 'connected',
-            'in_shape': [None, 784],
-            'out_shape': [None, 784],
-            'variables_shape': [784, 784],
-            'bias': True
-        },
-        {
-            'type': 'activation',
-            'function': tf.nn.softplus
-        },
-        {
-            'type': 'connected',
-            'in_shape': [None, 784],
-            'out_shape': [None, 10],
-            'variables_shape': [784, 10],
-            'bias': True
-        }
-    ],
-    'train_settings': {
-        'optimizer': tf.train.GradientDescentOptimizer(0.5),
-        'loss_function': tf.nn.softmax_cross_entropy_with_logits,
-        'reducer': tf.reduce_mean
-    },
-    'prediction_activation': tf.nn.softmax
-}
 
 print("models defined: {}".format(datetime.now()))
 
-simple_nn_tf = Tensorflow_neural_network(**simple_nn_model)
-tr_acc_simple, tst_acc_simple = simple_nn_tf.train(flat_images_train,
-                                                   labels_train,
-                                                   100,
-                                                   10,
-                                                   test_data=flat_images_test,
-                                                   test_labels=labels_test)
 
-simple_nn_accuracies = pd.DataFrame({'training_accuracy': tr_acc_simple, 'test_accuracy': tst_acc_simple})
+# simple_nn_tf = Tensorflow_neural_network(**simple_nn_model)
+# tr_acc_simple, tst_acc_simple = simple_nn_tf.train(flat_images_train,
+#                                                    labels_train,
+#                                                    100,
+#                                                    1,
+#                                                    test_data=flat_images_test,
+#                                                    test_labels=labels_test)
+#
+# simple_nn_accuracies = pd.DataFrame({'training_accuracy': tr_acc_simple, 'test_accuracy': tst_acc_simple})
 
-conv_nn_tf = Tensorflow_neural_network(**convolution_model)
+conv_nn_tf = TensorflowNeuralNetwork(**convolution_model,
+                                     regularization=True,
+                                     regularization_constant=0.1,
+                                     save_path="d:/kaggle/digits/tf_model_saves/tf_save_load_test"
+                                     )
 tr_acc_conv, tst_acc_conv = conv_nn_tf.train(images_train,
                                              labels_train,
                                              100,
-                                             10,
+                                             2,
                                              test_data=images_test,
-                                             test_labels=labels_test)
+                                             test_labels=labels_test,
+                                             )
+
+conv_nn_tf.store()
 
 conv_nn_accuracies = pd.DataFrame({'training_accuracy': tr_acc_conv, 'test_accuracy': tst_acc_conv})
 
-submit_images = pd.read_csv('d:/kaggle/digits/test.csv').values.reshape(-1,28,28,1)
+submit_images = pd.read_csv('d:/kaggle/digits/test.csv').values.reshape(-1,28,28,1)/255
 submit_example = pd.read_csv('d:/kaggle/digits/sample_submission.csv')
 submit_example.Label = conv_nn_tf.predict(submit_images)
 
+submit_example.to_csv("d:/kaggle/digits/save_load_test_output_results.csv", index=False)
--- tensorflow_network_creator.py
+++ tensorflow_network_creator.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 import tensorflow as tf
@@ -5,36 +6,53 @@ import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
 
-class Tensorflow_neural_network(object):
 
-    def _test_network_consistency(self):
-        # TODO
-        return True
+class TensorflowNeuralNetwork(object):
+    ACTIVATION_MAP = {
+        "relu": tf.nn.relu,
+        "softmax": tf.nn.softmax
+    }
+    OPTIMIZER_MAP = {
+        "GradientDescent": tf.train.GradientDescentOptimizer,
+        "Adam": tf.train.AdamOptimizer
+    }
+    OPTIMIZER_DECAY_MAP = {
+        "exponential": tf.train.exponential_decay,
+        "polynomial": tf.train.polynomial_decay
+    }
+    REDUCER_MAP = {
+        "mean": tf.reduce_mean
+    }
+    LOSS_FUNCTION_MAP = {
+        "softmax_cross_entropy": tf.nn.softmax_cross_entropy_with_logits
+    }
 
-    def _make_tf_layer(self, layer_info, in_variable):
+    def _make_tf_layer(self, layer_info, in_variable, layer_name):
         bias = None
         if 'variables_shape' in layer_info:
-            weights = tf.Variable(tf.truncated_normal(shape=layer_info['variables_shape'], stddev=0.1))
+            weights = tf.Variable(tf.truncated_normal(shape=layer_info['variables_shape'], stddev=0.1,
+                                                      name=layer_name+"weights_seed"),
+                                  name=layer_name+'weights')
             if layer_info.get('bias', False):
-                bias = tf.Variable(tf.constant(0.1, shape=layer_info['out_shape'][1:]))
+                bias = tf.Variable(tf.constant(0.1, shape=layer_info['out_shape'][1:], name=layer_name+"bias_seed"), name=layer_name+'bias')
         else:
             weights = None
         if layer_info['type'] == 'conv':
-            strides = layer_info.get('strides', [1,1,1,1])
+            strides = layer_info.get('strides', [1, 1, 1, 1])
             padding = layer_info.get('padding', 'SAME')
-            output = tf.nn.conv2d(in_variable, weights, strides, padding=padding)
+            output = tf.nn.conv2d(in_variable, weights, strides, padding=padding, name=layer_name+'output')
         elif layer_info['type'] == 'activation':
-            output = layer_info['function'](in_variable)
+            output = self.ACTIVATION_MAP[layer_info['function']](in_variable, name=layer_name+'output')
         elif layer_info['type'] == 'pool':
             ksize = layer_info.get('ksize', [1, 2, 2, 1])
             strides = layer_info.get('strides', [1, 2, 2, 1])
             padding = layer_info.get('padding', 'SAME')
-            output = tf.nn.max_pool(in_variable, ksize, strides, padding)
+            output = tf.nn.max_pool(in_variable, ksize, strides, padding, name=layer_name+'output')
         elif layer_info['type'] == 'reshape':
             new_shape = [x if x is not None else -1 for x in layer_info['out_shape']]
-            output = tf.reshape(in_variable, new_shape)
+            output = tf.reshape(in_variable, new_shape, name=layer_name+'output')
         elif layer_info['type'] == 'connected':
-            output = tf.matmul(in_variable, weights)
+            output = tf.matmul(in_variable, weights, name=layer_name+'output')
         else:
             raise ValueError("Unknown layer type: {}".format(layer_info['type']))
         return {
@@ -43,147 +61,144 @@ class Tensorflow_neural_network(object):
             'output': output
         }
 
-    def __init__(self, input_shape, output_shape, layers, train_settings, prediction_activation):
+    def __init__(self, input_shape, output_shape, layers, train_settings, prediction_activation,
+                 regularization=False, regularization_constant=None,
+                 save_path="./model{}".format(datetime.now()),
+                 op_name_prefix=""):
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.layers = layers
-        self.inputs = tf.placeholder(tf.float32, shape=layers[0]['in_shape'])
-        self.targets = tf.placeholder(tf.float32, shape=layers[-1]['out_shape'])
-        if not self._test_network_consistency():
-            raise ValueError("Network layers not consistent")
+        self.train_settings = train_settings
+        self.prediction_activation = prediction_activation
+        self.regularization = regularization
+        self.regularization_constant = regularization_constant
+        self.save_path = save_path
+        self.op_name_prefix = op_name_prefix
+
+        self.layer_names = [self.op_name_prefix+"layer_{}_".format(i) for i in range(len(layers))]
+
+        self.tf_global_step = tf.Variable(0, trainable=False, name=op_name_prefix+"global_step")
+        self.tf_inputs = tf.placeholder(tf.float32, shape=layers[0]['in_shape'], name=op_name_prefix+"tf_inputs")
+        self.tf_targets = tf.placeholder(tf.float32, shape=layers[-1]['out_shape'], name=op_name_prefix+"tf_targets")
         self.tf_layers = [None] * len(layers)
-        in_variable = self.inputs
+        in_variable = self.tf_inputs
         for i, layer in enumerate(self.layers):
-            self.tf_layers[i] = self._make_tf_layer(layer, in_variable)
+            self.tf_layers[i] = self._make_tf_layer(layer, in_variable, self.layer_names[i])
             in_variable = self.tf_layers[i]['output']
-        self.tf_loss = train_settings['reducer'](train_settings['loss_function'](logits=self.tf_layers[-1]['output'],
-                                                                                 labels=self.targets))
-        self.train_step = train_settings['optimizer'].minimize(self.tf_loss)
-        self.label_prediction = tf.argmax(prediction_activation(self.tf_layers[-1]['output']), 1)
-        self.accuracy = tf.reduce_mean(tf.to_float(tf.equal(self.label_prediction, tf.argmax(self.targets, 1))))
-        self.variable_initializer = tf.global_variables_initializer()
-        self.saver = tf.train.Saver()
+        self.tf_regularization_base = tf.constant(0.0, shape=(1,), name=op_name_prefix+"regularization_base")
+        if regularization:
+            weights = [x['weights'] for x in self.tf_layers if x['weights'] is not None]
+            if regularization_constant is None:
+                self.tf_regularization_constant = tf.constant(1.0, dtype=tf.float32,
+                                                           name=op_name_prefix+"regularization_constant")
+            else:
+                self.tf_regularization_constant = tf.constant(regularization_constant, dtype=tf.float32,
+                                                           name=op_name_prefix + "regularization_constant")
+            self.tf_regularization = tf.multiply(self.tf_regularization_constant,
+                                                 tf.add_n([tf.norm(weight) for weight in weights]),
+                                                 name=op_name_prefix+"regularization")
 
+        self.tf_loss_base = self.REDUCER_MAP[train_settings['reducer']](
+            self.LOSS_FUNCTION_MAP[train_settings['loss_function']](logits=self.tf_layers[-1]['output'],
+                                                                    labels=self.tf_targets),
+            name=op_name_prefix+"tf_loss_base")
+        self.tf_loss = tf.add(self.tf_loss_base, self.tf_regularization/tf.cast(tf.shape(self.tf_inputs)[0],
+                                                                                tf.float32,
+                                                                                name=op_name_prefix+"loss_cast"),
+                              name=op_name_prefix+"loss"
+                              )
 
+        optimizer = self.OPTIMIZER_MAP[train_settings['optimizer']]
+        if train_settings['learning_rate'].get("decay_args") is not None:
+            self.tf_learning_rate = self.OPTIMIZER_DECAY_MAP[train_settings[
+                'learning_rate']["decay"]](global_step=self.tf_global_step,
+                                           **train_settings['learning_rate']["decay_args"],
+                                           name=op_name_prefix+"learning_rate")
+        else:
+            self.tf_learning_rate = tf.constant(train_settings['learning_rate']['base'],
+                                                dtype=tf.float32,
+                                                name=op_name_prefix + "learning_rate")
 
+        self.tf_train_step = optimizer(self.tf_learning_rate).minimize(self.tf_loss,
+                                                                       global_step=self.tf_global_step,
+                                                                       name=op_name_prefix+"train_step")
+        self.tf_label_prediction = tf.argmax(self.ACTIVATION_MAP[prediction_activation](self.tf_layers[-1]['output']),
+                                             1,
+                                             name=op_name_prefix+"label_prediction")
+        self.tf_accuracy = tf.reduce_mean(tf.to_float(tf.equal(self.tf_label_prediction,
+                                                               tf.argmax(self.tf_targets, 1))),
+                                          name=op_name_prefix+"accuracy")
+        self.tf_variable_initializer = tf.global_variables_initializer()
+        self.tf_saver = tf.train.Saver(name=op_name_prefix+"saver")
+        self.tf_sess = tf.Session()
+
+    def store(self):
+        init_vars = {
+            'input_shape': self.input_shape,
+            'output_shape': self.output_shape,
+            'layers': self.layers,
+            'train_settings': self.train_settings,
+            'prediction_activation': self.prediction_activation,
+            'regularization': self.regularization,
+            'regularization_constant': self.regularization_constant,
+            'save_path': self.save_path,
+            'op_name_prefix': self.op_name_prefix
+        }
+        store_filename = self.save_path + "_meta.json"
+        with open(store_filename, 'w') as f:
+            f.write(json.dumps(init_vars))
+        self.tf_saver.save(self.tf_sess, save_path=self.save_path+"model")
+
+    @classmethod
+    def load(cls, path):
+        tf.reset_default_graph()
+        store_filename = path + "_meta.json"
+        with open(store_filename, 'r') as f:
+            init_vars = json.loads(f.read())
+        network = cls(**init_vars)
+        network.tf_sess.run(network.tf_variable_initializer)
+        network.tf_saver.restore(network.tf_sess, network.save_path+"model")
+        return network
 
     def train(self, training_data, training_labels, batch_size, epochs, warm_start=False,
-              save_path="d:/kaggle/digits/tf_model_saves/", test_data=None, test_labels=None):
+              test_data=None, test_labels=None):
         train_accuracy = []
         test_accuracy = []
         batches = int(training_data.shape[0]/batch_size)
-        full_feed_dict = {self.inputs: training_data, self.targets: training_labels}
-        test_feed_dict = {self.inputs: test_data, self.targets: test_labels}
-        with tf.Session() as sess:
-            sess.run(self.variable_initializer)
-            self.saver.save(sess, save_path=save_path, global_step=0)
-            for epoch in range(epochs):
-                print("starting epoch {}: {}".format(epoch, datetime.now()))
-                print("\tloss: {}".format(sess.run(self.tf_loss, feed_dict=full_feed_dict)))
-                training_data, training_labels = shuffle(training_data, training_labels)
-                print("\tstart training: {}".format(datetime.now()))
-                for i in range(batches):
+        full_feed_dict = {self.tf_inputs: training_data, self.tf_targets: training_labels}
+        test_feed_dict = {self.tf_inputs: test_data, self.tf_targets: test_labels}
 
-                    feed_dict = {self.inputs: training_data[(i*batch_size):((i+1)*batch_size), :],
-                                 self.targets: training_labels[(i*batch_size):((i+1)*batch_size)]}
-                    sess.run(self.train_step, feed_dict=feed_dict)
-                if training_data.shape[0] > batch_size*batches:
-                    feed_dict = {self.inputs: training_data[((i + 1) * batch_size):, :],
-                                self.targets: training_labels[((i + 1) * batch_size):]}
-                    sess.run(self.train_step, feed_dict=feed_dict)
-                print("\tdone training epoch {}: {}".format(epoch, datetime.now()))
-                train_accuracy.append(sess.run(self.accuracy, feed_dict=full_feed_dict))
-                if test_data is not None:
-                    test_accuracy.append(sess.run(self.accuracy, feed_dict=test_feed_dict))
+        self.tf_sess.run(self.tf_variable_initializer)
+        #self.tf_saver.save(self.tf_sess, save_path=self.save_path+"training", global_step=0)
+        loss = "undefined"
+        for epoch in range(epochs):
+            print("starting epoch {}: {}".format(epoch, datetime.now()))
+            print("\tloss: {}".format(loss))
+            training_data, training_labels = shuffle(training_data, training_labels)
+            print("\tstart training: {}".format(datetime.now()))
+            for i in range(batches):
 
-            print("\tloss: {}".format(sess.run(self.tf_loss, feed_dict=full_feed_dict)))
-            self.saver.save(sess, save_path=save_path, global_step=1)
+                feed_dict = {self.tf_inputs: training_data[(i*batch_size):((i+1)*batch_size), :],
+                             self.tf_targets: training_labels[(i*batch_size):((i+1)*batch_size)]}
+                _, loss = self.tf_sess.run([self.tf_train_step, self.tf_loss], feed_dict=feed_dict)
+            if training_data.shape[0] > batch_size*batches:
+                feed_dict = {self.tf_inputs: training_data[((i + 1) * batch_size):, :],
+                             self.tf_targets: training_labels[((i + 1) * batch_size):]}
+                _, loss = self.tf_sess.run([self.tf_train_step, self.tf_loss], feed_dict=feed_dict)
+            print("\tdone training epoch {}: {}".format(epoch, datetime.now()))
+            train_accuracy.append(self.tf_sess.run(self.tf_accuracy, feed_dict=full_feed_dict))
+            if test_data is not None:
+                test_accuracy.append(self.tf_sess.run(self.tf_accuracy, feed_dict=test_feed_dict))
+
+        print("\tloss: {}".format(self.tf_sess.run(self.tf_loss, feed_dict=full_feed_dict)))
+        #self.tf_saver.save(self.tf_sess, save_path=self.save_path+"training", global_step=1)
 
         return train_accuracy, test_accuracy
 
-
-
     def predict(self, test_data, save_path="d:/kaggle/digits/tf_model_saves/"):
         #this is broken because the session that trained ended and we lost the vars
-        feed_dict = {self.inputs: test_data}
-        with tf.Session() as sess:
-            self.saver.restore(sess, save_path=self.saver.last_checkpoints[-1])
-            result = sess.run(self.label_prediction, feed_dict=feed_dict)
+        feed_dict = {self.tf_inputs: test_data}
+        result = self.tf_sess.run(self.tf_label_prediction, feed_dict=feed_dict)
         return result
 
-if __name__ == '__main__':
-    # make convnet for the digit classifier
-    input_shape = [None, 28, 28, 1]
-    output_shape = [None, 10]
-    layers = [
-        {
-            'type': 'conv',
-            'in_shape': [None, 28, 28, 1],
-            'out_shape': [None, 28, 28, 8],
-            'variables_shape': [5, 5, 1, 8],
-            'bias': True
-        },
-        {
-            'type': 'activation',
-            'function': tf.nn.relu
-        },
-        {
-            'type': 'pool',
-            'in_shape': [None, 28, 28, 8],
-            'out_shape': [None, 14, 14, 8],
-            'ksize': [1, 2, 2, 1],
-            'strides': [1, 2, 2, 1]
-        },
-        {
-            'type': 'conv',
-            'in_shape': [None, 14, 14, 8],
-            'out_shape': [None, 14, 14, 8],
-            'variables_shape': [5, 5, 8, 8],
-            'bias': True
-        },
-        {
-            'type': 'activation',
-            'function': tf.nn.relu
-        },
-        {
-            'type': 'reshape',
-            'in_shape': [None, 14, 14, 8],
-            'out_shape': [None, 14*14*8],
-        },
-        {
-            'type': 'connected',
-            'in_shape': [None, 14*14*8],
-            'out_shape': [None, 10],
-            'variables_shape': [14*14*8, 10],
-            'bias': True
-        }
-    ]
-    train_settings = {
-        'optimizer': tf.train.GradientDescentOptimizer(0.5),
-        'loss_function': tf.nn.softmax_cross_entropy_with_logits,
-        'reducer': tf.reduce_mean
-    }
-    prediction_activation = tf.nn.softmax
-    training_data = pd.read_csv('d:/kaggle/digits/train.csv')
-    def one_hot_digits(x):
-        res = np.zeros((10,), dtype=float)
-        res[x] = 1.0
-        return(res)
-    labels = np.apply_along_axis(one_hot_digits, axis=1, arr=training_data[['label']])
-    images = training_data.loc[:, 'pixel0':'pixel783'].values.reshape(-1, 28, 28, 1)/255
-    images, labels = shuffle(images, labels)
-    test_proportion = 0.1
-    images_test = images[:int(test_proportion*images.shape[0])]
-    images_train = images[int(test_proportion*images.shape[0]):]
-    labels_test = labels[:int(test_proportion * images.shape[0])]
-    labels_train = labels[int(test_proportion * images.shape[0]):]
-
-    test_neural_net = Tensorflow_neural_network(input_shape, output_shape, layers, train_settings, prediction_activation)
-    tr_acc, tst_acc = test_neural_net.train(images_train, labels_train, 100, 50, test_data=images_test, test_labels=labels_test)
-    submission_set = pd.read_csv('d:/kaggle/digits/test.csv')
-    submission_imgs = submission_set.values.reshape(-1, 28, 28, 1)/255
-    submission_vals = test_neural_net.predict(submission_imgs)
-    df = pd.read_csv("d:/kaggle/digits/sample_submission.csv")
-    df.Label = submission_vals
-    df.to_csv("d:/kaggle/digits/sub_tf_nn_extended.py", index=False)
 
